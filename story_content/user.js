5865c6cfde0d33edd04b4f19147e8465adeb2d2e
Karlstan <82640515+Karlstan@users.noreply.github.com>
2021-04-19 16:24:00 +0800

    MID942 - Understanding Virus
    MID942 - Multimedia Design for NIE Masters. The title of the courseware is Understanding Virus.

---
+++ story_content/user.js
@@ -0,0 +1,175 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5Y5amKGzSSv":
+        Script1();
+        break;
+      case "63uDFxUgd8E":
+        Script2();
+        break;
+      case "6g4qjJF8pKW":
+        Script3();
+        break;
+      case "5XZ1vxwSCbJ":
+        Script4();
+        break;
+      case "5fuCgQR1xkM":
+        Script5();
+        break;
+      case "6eBM6tg8KIs":
+        Script6();
+        break;
+      case "5fsFnkc9Ywd":
+        Script7();
+        break;
+  }
+}
+
+function Script1()
+{
+  var test = document.getElementById("MyStyle");
+if(!test){
+ var body = document.querySelector("body");
+ var style = document.createElement('style');
+ style.type = 'text/css';
+ style.id = 'MyStyle';
+ style.appendChild(document.createTextNode(".caption p { background: rgba(236,212,76,0.7); color: black; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+ body.appendChild(style);
+}
+
+if (!window.movedCaptions) {
+ var sheet = document.createElement('style');
+ sheet.innerHTML = ".caption{margin-bottom:45px !important; margin-left:75px !important }";
+/*margin-bottom: 65px - 45px ; margin-left: 90px - 75px*/
+ document.body.appendChild(sheet);
+ window.movedCaptions = true;
+}
+}
+
+function Script2()
+{
+  var test = document.getElementById("MyStyle");
+if(!test){
+ var body = document.querySelector("body");
+ var style = document.createElement('style');
+ style.type = 'text/css';
+ style.id = 'MyStyle';
+ style.appendChild(document.createTextNode(".caption p { background: rgba(236,212,76,0.7); color: black; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+ body.appendChild(style);
+}
+
+if (!window.movedCaptions) {
+ var sheet = document.createElement('style');
+ sheet.innerHTML = ".caption{margin-bottom:45px !important; margin-left:75px !important }";
+/*margin-bottom: 65px - 45px ; margin-left: 90px - 75px*/
+ document.body.appendChild(sheet);
+ window.movedCaptions = true;
+}
+}
+
+function Script3()
+{
+  var test = document.getElementById("MyStyle");
+if(!test){
+ var body = document.querySelector("body");
+ var style = document.createElement('style');
+ style.type = 'text/css';
+ style.id = 'MyStyle';
+ style.appendChild(document.createTextNode(".caption p { background: rgba(236,212,76,0.7); color: black; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+ body.appendChild(style);
+}
+
+if (!window.movedCaptions) {
+ var sheet = document.createElement('style');
+ sheet.innerHTML = ".caption{margin-bottom:45px !important; margin-left:75px !important }";
+/*margin-bottom: 65px - 45px ; margin-left: 90px - 75px*/
+ document.body.appendChild(sheet);
+ window.movedCaptions = true;
+}
+}
+
+function Script4()
+{
+  var test = document.getElementById("MyStyle");
+if(!test){
+ var body = document.querySelector("body");
+ var style = document.createElement('style');
+ style.type = 'text/css';
+ style.id = 'MyStyle';
+ style.appendChild(document.createTextNode(".caption p { background: rgba(236,212,76,0.7); color: black; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+ body.appendChild(style);
+}
+
+if (!window.movedCaptions) {
+ var sheet = document.createElement('style');
+ sheet.innerHTML = ".caption{margin-bottom:45px !important; margin-left:75px !important }";
+/*margin-bottom: 65px - 45px ; margin-left: 90px - 75px*/
+ document.body.appendChild(sheet);
+ window.movedCaptions = true;
+}
+}
+
+function Script5()
+{
+  var test = document.getElementById("MyStyle");
+if(!test){
+ var body = document.querySelector("body");
+ var style = document.createElement('style');
+ style.type = 'text/css';
+ style.id = 'MyStyle';
+ style.appendChild(document.createTextNode(".caption p { background: rgba(236,212,76,0.7); color: black; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+ body.appendChild(style);
+}
+
+if (!window.movedCaptions) {
+ var sheet = document.createElement('style');
+ sheet.innerHTML = ".caption{margin-bottom:45px !important; margin-left:75px !important }";
+/*margin-bottom: 65px - 45px ; margin-left: 90px - 75px*/
+ document.body.appendChild(sheet);
+ window.movedCaptions = true;
+}
+}
+
+function Script6()
+{
+  var test = document.getElementById("MyStyle");
+if(!test){
+ var body = document.querySelector("body");
+ var style = document.createElement('style');
+ style.type = 'text/css';
+ style.id = 'MyStyle';
+ style.appendChild(document.createTextNode(".caption p { background: rgba(236,212,76,0.7); color: black; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+ body.appendChild(style);
+}
+
+if (!window.movedCaptions) {
+ var sheet = document.createElement('style');
+ sheet.innerHTML = ".caption{margin-bottom:45px !important; margin-left:75px !important }";
+/*margin-bottom: 65px - 45px ; margin-left: 90px - 75px*/
+ document.body.appendChild(sheet);
+ window.movedCaptions = true;
+}
+}
+
+function Script7()
+{
+  var test = document.getElementById("MyStyle");
+if(!test){
+ var body = document.querySelector("body");
+ var style = document.createElement('style');
+ style.type = 'text/css';
+ style.id = 'MyStyle';
+ style.appendChild(document.createTextNode(".caption p { background: rgba(236,212,76,0.7); color: black; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+ body.appendChild(style);
+}
+
+if (!window.movedCaptions) {
+ var sheet = document.createElement('style');
+ sheet.innerHTML = ".caption{margin-bottom:45px !important; margin-left:75px !important }";
+/*margin-bottom: 65px - 45px ; margin-left: 90px - 75px*/
+ document.body.appendChild(sheet);
+ window.movedCaptions = true;
+}
+}
+
